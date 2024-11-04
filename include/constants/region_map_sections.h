61e8edb4ea8b8dd08f7c941693848c54d6f1fb9f
shachar700 <shachar700@gmail.com>
2024-11-04 18:30:09 +0200

    Removed squares in map layout

--- include/constants/region_map_sections.h
+++ include/constants/region_map_sections.h
@@ -218,7 +218,8 @@
 #define MAPSEC_PTOLEMAIS_CITY               0xD6
 #define MAPSEC_ROUTE_A                      0xD7
 #define MAPSEC_ROUTE_B                      0xD8
-#define MAPSEC_NONE                         0xD9
+#define MAPSEC_SODORI_TUNNELS               0xD9
+#define MAPSEC_NONE                         0xDA
 
 #define METLOC_SPECIAL_EGG                  0xFD
 #define METLOC_IN_GAME_TRADE                0xFE
