327c9f5c822c8a19a352497278d5f44613e73868
shachar700 <shachar700@gmail.com>
2024-12-03 11:20:07 +0200

    setting pokemon in pokedex area defined

--- src/pokedex_area_screen.c
+++ src/pokedex_area_screen.c
@@ -30,8 +30,8 @@
 //   determined by the data for the corresponding MAPSEC in gRegionMapEntries.
 
 // Only maps in the following map groups have their encounters considered for the area screen
-#define MAP_GROUP_TOWNS_AND_ROUTES MAP_GROUP(PETALBURG_CITY)
-#define MAP_GROUP_DUNGEONS MAP_GROUP(METEOR_FALLS_1F_1R)
+#define MAP_GROUP_TOWNS_AND_ROUTES MAP_GROUP(HAWAM_CITY) //PETALBURG_CITY
+#define MAP_GROUP_DUNGEONS MAP_GROUP(KEREN_TUNNELS) //METEOR_FALLS_1F_1R
 #define MAP_GROUP_SPECIAL_AREA MAP_GROUP(SAFARI_ZONE_NORTHWEST)
 
 #define AREA_SCREEN_WIDTH 32
