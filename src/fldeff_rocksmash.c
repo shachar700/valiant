f52cbb3531f432f32782fe66c9f709b59b3dd2a6
shachar700 <shachar700@gmail.com>
2024-12-04 16:32:03 +0200

    fixed cut field animation bug

--- src/fldeff_rocksmash.c
+++ src/fldeff_rocksmash.c
@@ -61,7 +61,7 @@ static void Task_DoFieldMove_Init(u8 taskId)
     if (!ObjectEventIsMovementOverridden(&gObjectEvents[objEventId])
      || ObjectEventClearHeldMovementIfFinished(&gObjectEvents[objEventId]))
     {
-        if (gMapHeader.mapType == MAP_TYPE_UNDERWATER || gFieldEffectArguments[3])
+        if (gMapHeader.mapType == MAP_TYPE_UNDERWATER /*|| gFieldEffectArguments[3]*/)
         {
             // Skip field move pose underwater, or if arg3 is nonzero
             if (gFieldEffectArguments[3])
